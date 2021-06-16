f2b5a61d3fd9ffdd90665d56beaaac04a289e227
MatthewTGM <matthewtgm120@gmail.com>
2021-06-16 12:57:59 +0200

    Please tell me the type adapters are fixed?

--- src/main/java/xyz/matthewtgm/json/gson/adapters/JsonArrayTypeAdapter.java
+++ src/main/java/xyz/matthewtgm/json/gson/adapters/JsonArrayTypeAdapter.java
@@ -4,21 +4,16 @@ import com.google.gson.*;
 import xyz.matthewtgm.json.objects.JsonArray;
 
 import java.lang.reflect.Type;
-import java.util.Arrays;
 import java.util.List;
 
 public class JsonArrayTypeAdapter implements JsonDeserializer<JsonArray>, JsonSerializer<JsonArray> {
 
     public JsonArray deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return new JsonArray(convertToList(json.toString()));
+        return new JsonArray(new GsonBuilder().setLenient().create().fromJson(json.toString(), List.class));
     }
 
     public JsonElement serialize(JsonArray src, Type typeOfSrc, JsonSerializationContext context) {
         return JsonParser.parseString(src.toJson());
-    }
-
-    private List convertToList(String input) {
-        return Arrays.asList(input);
     }
 
 }
--- src/main/java/xyz/matthewtgm/json/gson/adapters/JsonObjectTypeAdapter.java
+++ src/main/java/xyz/matthewtgm/json/gson/adapters/JsonObjectTypeAdapter.java
@@ -10,22 +10,11 @@ import java.util.Map;
 public class JsonObjectTypeAdapter implements JsonDeserializer<JsonObject>, JsonSerializer<JsonObject> {
 
     public JsonObject deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return new JsonObject(convertToMap(json.toString()));
+        return new JsonObject(new GsonBuilder().setLenient().create().fromJson(json.toString(), Map.class));
     }
 
     public JsonElement serialize(JsonObject src, Type typeOfSrc, JsonSerializationContext context) {
         return JsonParser.parseString(src.toJson());
-    }
-
-    private Map convertToMap(String input) {
-        input = input.substring(1, input.length()-1);
-        String[] keyValuePairs = input.split(",");
-        Map<String,String> map = new HashMap<>();
-        for(String pair : keyValuePairs) {
-            String[] entry = pair.split("=");
-            map.put(entry[0].trim(), entry[1].trim());
-        }
-        return map;
     }
 
 }
