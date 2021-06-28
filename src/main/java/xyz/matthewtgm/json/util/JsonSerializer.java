3940d0bbd018ef8e9cdfd2574358045146bf421a
MatthewTGM <matthewtgm120@gmail.com>
2021-06-28 15:13:26 +0200

    Added "empty" checks to serialization.

--- src/main/java/xyz/matthewtgm/json/util/JsonSerializer.java
+++ src/main/java/xyz/matthewtgm/json/util/JsonSerializer.java
@@ -8,6 +8,7 @@ import xyz.matthewtgm.json.objects.JsonObject;
 
 import java.io.File;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class JsonSerializer {
 
@@ -41,12 +42,29 @@ public class JsonSerializer {
                 if (field.isAnnotationPresent(JsonSerializeName.class)) name = field.getAnnotation(JsonSerializeName.class).value();
                 Object value = field.get(instance);
                 if (value == null) continue;
+                if (checkEmpty(value) == null) continue;
                 json.add(name, value);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return json;
+    }
+
+    private static Object checkEmpty(Object value) {
+        Object val;
+        try {
+            Class<?> valueClazz = value.getClass();
+            Method emptyMethod = valueClazz.getDeclaredMethod("isEmpty");
+            Object invoke = emptyMethod.invoke(value);
+            Class<?> invokeClazz = invoke.getClass();
+            if (invokeClazz.getSimpleName().toLowerCase().contains("bool")) val = invoke;
+            else val = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            val = null;
+        }
+        return val;
     }
 
     private static String fixFileName(String fileName) {
