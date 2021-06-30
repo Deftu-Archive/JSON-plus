31e271f24d426d5ae6acdc8b31fa1b17bc7db1ee
MatthewTGM <matthewtgm120@gmail.com>
2021-06-30 11:33:56 +0200

    Added author and since to JsonElement javadocs.

--- src/main/java/xyz/matthewtgm/json/entities/JsonElement.java
+++ src/main/java/xyz/matthewtgm/json/entities/JsonElement.java
@@ -7,55 +7,81 @@ public abstract class JsonElement {
 
     /**
      * @return A copy of the element with all it's values.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract JsonElement copy();
 
     /**
      * @return The element as a JsonObject.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract JsonObject getAsJsonObject();
     /**
      * @return The element as a JsonArray.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract JsonArray getAsJsonArray();
     /**
      * @return The element as a JsonPrimitive.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract JsonPrimitive getAsJsonPrimitive();
     /**
      * @return The element as a long.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract long getAsLong();
     /**
      * @return The element as an int.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract int getAsInt();
     /**
      * @return The element as a double.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract double getAsDouble();
     /**
      * @return The element as a float.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract float getAsFloat();
     /**
      * @return The element as a byte.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract byte getAsByte();
     /**
      * @return The element as a short.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract short getAsShort();
     /**
      * @return The element as a char.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract char getAsChar();
     /**
      * @return The element as a boolean.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract boolean getAsBoolean();
     /**
      * @return The element as a String.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public String getAsString() {
         return toString();
@@ -63,6 +89,8 @@ public abstract class JsonElement {
 
     /**
      * @return A String representation of the element.
+     * @author MatthewTGM
+     * @since 2.1
      */
     public abstract String toString();
 
